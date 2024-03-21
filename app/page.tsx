7353edec1020a3b6378b89ecf62c0dc79c1d556b
Nick Golitsyn <45421041+NickGolitsyn@users.noreply.github.com>
2024-03-21 18:01:54 +0000

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -30,30 +30,10 @@ export default function Home() {
   ]
   return (
     <div className="flex flex-col min-h-[100dvh]">
-    <header className="px-4 lg:px-6 h-14 flex items-center">
-      <Link className="flex items-center justify-center" href="#">
-        <MountainIcon className="h-6 w-6" />
-        <span className="sr-only">Gleb Feels</span>
-      </Link>
-      <nav className="ml-auto flex gap-4 sm:gap-6">
-        <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
-          Features
-        </Link>
-        <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
-          Pricing
-        </Link>
-        <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
-          About
-        </Link>
-        <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
-          Contact
-        </Link>
-      </nav>
-    </header>
     <main className="flex-1">
-      <section className="w-full py-12 px-10">
-        <h1 className="text-3xl font-bold uppercase text-center px-10">How to move from the city to the countryside</h1>
-        <p className="text-neutral-500 text-center my-5">
+      <section className="w-full px-5 py-12 md:px-10">
+        <h1 className="text-3xl font-bold uppercase text-center">How to move from the city to the countryside</h1>
+        <p className="text-neutral-500 text-center my-5 max-w-[600px] mx-auto">
         Embark on a transformative journey from the bustling city streets to the tranquil expanse of the countryside. This concise guide offers practical advice and step-by-step instructions for those dreaming of a simpler, more sustainable life amidst nature. Learn how to seamlessly transition to rural living, from selecting your new home to cultivating your own food and integrating into the countryside. Discover the essentials of growing produce, raising animals, and choosing the right tools to navigate your new lifestyle. Whether you are drawn to the peace of rural spaces or the independence of self-sufficiency, this book illuminates the path to achieving your countryside living dreams.
         </p>
         <h2 className="text-2xl font-semibold mb-3 text-center">Choose Your Book</h2>
@@ -63,7 +43,7 @@ export default function Home() {
               <div className="flex justify-center">
                 <Image 
                   src={cover} 
-                  className="rounded-sm max-w-40 h-fit"
+                  className="rounded-sm max-w-48 h-fit"
                   alt={"Book cover"}
                 />
               </div>
@@ -96,11 +76,11 @@ export default function Home() {
           ))}
         </div>
       </section>
-      <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-neutral-100 dark:bg-neutral-800">
         <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6">
           <div className="space-y-3">
             <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">About the Author</h2>
-            <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+            <p className="mx-auto max-w-[600px] text-neutral-500">
             Gleb Feels, a Serbian influencer renowned for his lifestyle shift from London&apos;s hustle to Serbia&apos;s tranquil countryside, shares his journey through TikTok and Instagram. Embracing rural life, Gleb advocates for self-sufficiency, from cultivating food to coexisting with nature. His book, &quot;Moving from the City to the Countryside,&quot; distills his experiences into practical advice for others seeking a similar transformation. Gleb&apos;s narrative inspires a deeper connection with the environment and a more fulfilling way of life.
             </p>
           </div>
@@ -112,7 +92,7 @@ export default function Home() {
             <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
               Praise for The Art of Creativity
             </h2>
-            <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+            <p className="mx-auto max-w-[600px] text-neutral-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-neutral-400">
               "This book is a game changer. It opened my mind to new possibilities and gave me the tools to turn my
               ideas into reality."
             </p>
@@ -122,7 +102,7 @@ export default function Home() {
               <Input className="max-w-lg flex-1" placeholder="Enter your email" type="email" />
               <Button>Preorder Now</Button>
             </form>
-            <p className="text-xs text-gray-500 dark:text-gray-400">
+            <p className="text-xs text-neutral-500 dark:text-neutral-400">
               Sign up to get notified when we launch.
               <Link className="underline underline-offset-2" href="#">
                 Terms & Conditions
@@ -133,7 +113,7 @@ export default function Home() {
       </section> */}
     </main>
     <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-      <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Acme Inc. All rights reserved.</p>
+      <p className="text-xs text-neutral-500 dark:text-neutral-400">© 2024 Acme Inc. All rights reserved.</p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
         <Link className="text-xs hover:underline underline-offset-4" href="#">
           Terms of Service
@@ -145,23 +125,4 @@ export default function Home() {
     </footer>
   </div>
   );
-}
-
-function MountainIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
-  )
 }
