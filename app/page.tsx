85e09d866993d3b4a44e78739cfbb4f8efbc075f
Nick Golitsyn <45421041+NickGolitsyn@users.noreply.github.com>
2024-03-21 17:41:57 +0000

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,167 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import cover from "@/public/cover.jpg"
 import Image from "next/image";
 
 export default function Home() {
+  let books = [
+    {
+      title: 'Paperback',
+      price: '£19.90',
+      status: 'Pre-order now',
+      link: 'https://buy.stripe.com/test_7sI9Ca5i8e2k14A001',
+      availability: true
+    },
+    {
+      title: 'Hardcover',
+      price: '£39.90',
+      status: 'Out of stock',
+      link: 'https://buy.stripe.com/test_7sI9Ca5i8e2k14A001',
+      availability: false
+    },
+    {
+      title: 'e-Book',
+      price: '£9.90',
+      status: 'Buy now',
+      link: 'https://buy.stripe.com/test_7sI9Ca5i8e2k14A001',
+      availability: true
+    }
+  ]
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <div className="flex flex-col min-h-[100dvh]">
+    <header className="px-4 lg:px-6 h-14 flex items-center">
+      <Link className="flex items-center justify-center" href="#">
+        <MountainIcon className="h-6 w-6" />
+        <span className="sr-only">Gleb Feels</span>
+      </Link>
+      <nav className="ml-auto flex gap-4 sm:gap-6">
+        <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+          Features
+        </Link>
+        <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+          Pricing
+        </Link>
+        <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+          About
+        </Link>
+        <Link className="text-sm font-medium hover:underline underline-offset-4" href="#">
+          Contact
+        </Link>
+      </nav>
+    </header>
+    <main className="flex-1">
+      <section className="w-full py-12 px-10">
+        <h1 className="text-3xl font-bold uppercase text-center px-10">How to move from the city to the countryside</h1>
+        <p className="text-neutral-500 text-center my-5">
+        Embark on a transformative journey from the bustling city streets to the tranquil expanse of the countryside. This concise guide offers practical advice and step-by-step instructions for those dreaming of a simpler, more sustainable life amidst nature. Learn how to seamlessly transition to rural living, from selecting your new home to cultivating your own food and integrating into the countryside. Discover the essentials of growing produce, raising animals, and choosing the right tools to navigate your new lifestyle. Whether you're drawn to the peace of rural spaces or the independence of self-sufficiency, this book illuminates the path to achieving your countryside living dreams.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <h2 className="text-2xl font-semibold mb-3 text-center">Choose Your Book</h2>
+        <div className="flex justify-around flex-wrap gap-5">
+          {books.map((e, index) => (
+            <div key={index}>
+              <div className="flex justify-center">
+                <Image 
+                  src={cover} 
+                  className="rounded-sm max-w-40 h-fit"
+                  alt={"Book cover"}
+                />
+              </div>
+              <div className="max-w-[500px] max-md:mx-auto flex flex-col justify-center space-y-1 lg:col-span-2 xl:space-y-2">
+                <div className="space-y-2">
+                  <h1 className="text-xl font-medium">
+                    {e.title}
+                  </h1>
+                </div>
+                <div className="font-medium text-neutral-500">
+                  <span>{e.price}</span>
+                </div>
+                <div>
+                  {e.availability ? (
+                    <Button asChild>
+                      <Link href={e.link} className="w-full mt-3">
+                        {e.status}
+                      </Link>
+                    </Button>
+                  ) : (
+                    <Button variant="secondary" asChild>
+                      <span className="w-full mt-3 cursor-default">
+                        {e.status}
+                      </span>
+                    </Button>
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
+        <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6">
+          <div className="space-y-3">
+            <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">About the Author</h2>
+            <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+            Gleb Feels, a Serbian influencer renowned for his lifestyle shift from London's hustle to Serbia's tranquil countryside, shares his journey through TikTok and Instagram. Embracing rural life, Gleb advocates for self-sufficiency, from cultivating food to coexisting with nature. His book, "Moving from the City to the Countryside," distills his experiences into practical advice for others seeking a similar transformation. Gleb's narrative inspires a deeper connection with the environment and a more fulfilling way of life.
+            </p>
+          </div>
+        </div>
+      </section>
+      {/* <section className="w-full py-12 md:py-24 lg:py-32">
+        <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6">
+          <div className="space-y-3">
+            <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
+              Praise for The Art of Creativity
+            </h2>
+            <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+              "This book is a game changer. It opened my mind to new possibilities and gave me the tools to turn my
+              ideas into reality."
+            </p>
+          </div>
+          <div className="mx-auto w-full max-w-sm space-y-2">
+            <form className="flex space-x-2">
+              <Input className="max-w-lg flex-1" placeholder="Enter your email" type="email" />
+              <Button>Preorder Now</Button>
+            </form>
+            <p className="text-xs text-gray-500 dark:text-gray-400">
+              Sign up to get notified when we launch.
+              <Link className="underline underline-offset-2" href="#">
+                Terms & Conditions
+              </Link>
+            </p>
+          </div>
+        </div>
+      </section> */}
     </main>
+    <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+      <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Acme Inc. All rights reserved.</p>
+      <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+        <Link className="text-xs hover:underline underline-offset-4" href="#">
+          Terms of Service
+        </Link>
+        <Link className="text-xs hover:underline underline-offset-4" href="#">
+          Privacy
+        </Link>
+      </nav>
+    </footer>
+  </div>
   );
+}
+
+function MountainIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
+    </svg>
+  )
 }
