26c9284eb8cae4ad79f82e09ab6b50fdb034521b
Nick Golitsyn <45421041+NickGolitsyn@users.noreply.github.com>
2024-09-20 11:57:15 +0100

    cart

---
+++ data/products.tsx
@@ -0,0 +1,25 @@
+import { Product } from '../types';
+
+export const products: Product[] = [
+  {
+    id: 'prod_1',
+    name: 'Product 1',
+    price: 1500,
+    description: 'This is the description for product 1',
+    image: '/images/product1.jpg',
+  },
+  {
+    id: 'prod_2',
+    name: 'Product 2',
+    price: 2500, 
+    description: 'This is the description for product 2',
+    image: '/images/product2.jpg',
+  },
+  {
+    id: 'prod_3',
+    name: 'Product 3',
+    price: 3000,
+    description: 'This is the description for product 3',
+    image: '/images/product3.jpg',
+  },
+];
